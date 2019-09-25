3a53bb738c6f4bf48f2192116d1d6e9af5308c8a
Hunter <hunterkenneth422@gmail.com>
2019-09-24 19:40:47 -0600

    add code
     for assignment page 58

---
+++ Basic_JavaScirpt_Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,21 @@
+window.alert("Hello, World!");
+document.write("Just Saying, \"hello there, General Kenobi.\"");
+var A = "<br>wassup";
+document.write(A);
+document.write("<br>\For beautiful eyes, look for the good in others; for beautiful lips," 
++"<br>speak only words of kindness;" 
++"<br>and for poise, walk with the knowledge that you are never alone.\"")
+var B = "<br><br>what is up, "
++"my guy"
+document.write(B)
+var Food = "Pizza", Game = "<br>Skyrim", Drink = "Coffee", Movie = "Star Wars";
+document.write(Game)
+var myFavoriteColor = "<br>My favorite color is green.";
+var myFavoriteColor = myFavoriteColor.fontcolor("green");
+document.write(myFavoriteColor)
+document.write(3 + 3);
+function myFirstFunction() {
+    var str = "I hate the color red!";
+    var result = str.fontcolor("red");
+    document.getElementById("Red_Text").innerHTML = result;
+}
